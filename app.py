8460cfdfd8e8848575d777c0b52a3648e6e473a8
hasithakancherla <hasithakancherla0604@gmail.com>
2026-06-17 20:45:37 +0530

    Started Version 4 authentication models

--- app.py
+++ app.py
@@ -10,12 +10,61 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Expense(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    amount = db.Column(db.Integer, nullable=False)
-    category = db.Column(db.String(50), nullable=False)
+class User(db.Model):
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
 
+    username = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    email = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    password = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+
+class Expense(db.Model):
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    name = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    amount = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    category = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id'),
+        nullable=False
+    )
+
+user_id = db.Column(
+    db.Integer,
+    db.ForeignKey('user.id')
+)
 
 with app.app_context():
     db.create_all()
